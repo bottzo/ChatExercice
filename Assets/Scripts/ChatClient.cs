9ea63b0bcaf044861f194dc65e8d5bc4f6c90547
bottzo <45202057+bottzo@users.noreply.github.com>
2021-11-14 20:33:06 +0100

    basics done

---
+++ Assets/Scripts/ChatClient.cs
@@ -0,0 +1,245 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
+using System.IO;
+
+public class ChatClient : MonoBehaviour
+{
+    private enum ClientState
+    {
+        ServerNotAviable,
+        Disconected,
+        Connecting,
+        UnamedConnected,
+        Connected
+    }
+    ClientState state = ClientState.Disconected;
+    Socket clientSocket;
+    IPEndPoint serverIpep;
+    string username = "";
+    //public InputField input;
+    public Text outputLog;
+    public InputField inputField;
+    private Text placeHolderText;
+    public Button inputButton;
+    bool serverNotActive = false;
+    object notActiveLock = new object();
+    public float ReconectTime = 2.0f;
+    private float currentReconnectTimer;
+
+    private void Start()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverIpep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9052);
+        outputLog.text = "";
+        placeHolderText = inputField.placeholder.gameObject.GetComponent<Text>();
+    }
+    private void Update()
+    {
+        switch (state)
+        {
+            case ClientState.ServerNotAviable:
+                currentReconnectTimer -= Time.deltaTime;
+                if (currentReconnectTimer < 0.0f)
+                    state = ClientState.Disconected;
+                break;
+
+            case ClientState.Disconected:
+                inputField.interactable = false;
+                placeHolderText.text = "Connecting";
+                inputButton.interactable = false;
+                OutputChat("Connecting to server");
+                Thread connectingThread = new Thread(ConnectToserver);
+                connectingThread.Start();
+                state = ClientState.Connecting;
+                break;
+
+                case ClientState.Connecting:
+                lock (notActiveLock)
+                {
+                    if(serverNotActive == true)
+                    {
+                        state = ClientState.ServerNotAviable;
+                        serverNotActive = false;
+                        currentReconnectTimer = ReconectTime;
+                        OutputChat("Server not aviable");
+                        OutputChat("Trying reconection in " + ReconectTime + " seconds");
+                        break;
+                    }
+                }
+                if (clientSocket.Poll(0, SelectMode.SelectWrite))
+                {
+                    inputField.interactable = true;
+                    placeHolderText.text = "Enter Username";
+                    inputButton.interactable = true;
+                    state = ClientState.UnamedConnected;
+                    OutputChat("Succesfully conected to server");
+                    OutputChat("EnterUsername");
+                }
+                break;
+
+            case ClientState.UnamedConnected:
+                if (clientSocket.Poll(0, SelectMode.SelectRead))
+                {
+                    byte[] data = new byte[2048];
+                    int recv = clientSocket.Receive(data);
+                    if (recv == 0)
+                    {
+                        OutputChat("Lost Connection to server");
+                        state = ClientState.Disconected;
+                        break;
+                    }
+                    MemoryStream stream = new MemoryStream(data);
+                    BinaryReader reader = new BinaryReader(stream);
+                    string json;
+                    //string json = Encoding.ASCII.GetString(data, 0, recv);
+                    while (true)
+                    {
+                        json = reader.ReadString();
+                        if (json == "")
+                            break;
+
+                        Message msg = new Message();
+                        msg = JsonUtility.FromJson<Message>(json);
+                        if (msg.Type == Message.MessageType.ClientNameAccepted)
+                        {
+                            username = msg.message;
+                            OutputChat("Name " + username + " accepted!!!!");
+                            OutputChat("Welcome to the chat");
+                            state = ClientState.Connected;
+                            placeHolderText.text = "Enter message/command";
+                            //Posar aki totes les commands k el server accepta
+                        }
+                        else if (msg.Type == Message.MessageType.ClientNameRejected)
+                        {
+                            OutputChat("Username " + msg.message + " rejected");
+                            OutputChat("Username already exists or it starts with /");
+                            OutputChat("Try another name");
+                            placeHolderText.text = "Enter another Username";
+                        }
+                    }
+                }
+                break;
+
+            case ClientState.Connected:
+                if (clientSocket.Poll(0, SelectMode.SelectRead))
+                {
+                    byte[] data = new byte[2048];
+                    int recv = clientSocket.Receive(data);
+                    if (recv == 0)
+                    {
+                        OutputChat("Lost Connection to server");
+                        state = ClientState.Disconected;
+                        break;
+                    }
+                    MemoryStream stream = new MemoryStream(data);
+                    BinaryReader reader = new BinaryReader(stream);
+                    string json;
+                    //string json = Encoding.ASCII.GetString(data, 0, recv);
+                    while (true)
+                    {
+                        json = reader.ReadString();
+                        if (json == "")
+                            break;
+
+                        Message msg = new Message();
+                        msg = JsonUtility.FromJson<Message>(json);
+                        switch(msg.Type)
+                        {
+                            case Message.MessageType.ClientBroadcastedMessage:
+                                OutputChat(msg.message, msg.senderName);
+                                break;
+                            case Message.MessageType.ClientNewClientOnChat:
+                                OutputChat(msg.message + " connected to the chat");
+                                break;
+                            case Message.MessageType.ClientClientLeavedChat:
+                                OutputChat(msg.message + " disconected from the chat");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                break;
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        //jsut shutdown if we have a connection with the server
+        if(state != ClientState.ServerNotAviable && state != ClientState.Disconected && state !=ClientState.Connecting)
+            clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+        Debug.Log("bye :)");
+    }
+    private void JSONSerializeAndSendMessage(Message message, Socket socket)
+    {
+        //The endiannes???
+        //chack with the poll if we can send the message: what happens if we can't????
+        string json = JsonUtility.ToJson(message);
+        MemoryStream stream = new MemoryStream();
+        BinaryWriter writer = new BinaryWriter(stream);
+        writer.Write(json);
+        //Encoding.UTF8.GetBytes(json)
+        socket.Send(stream.GetBuffer());
+        //socket.Send(Encoding.ASCII.GetBytes(json));
+        IPEndPoint clientEndPoint = (IPEndPoint)socket.RemoteEndPoint;
+        Debug.Log("Sending message to client with address: " + clientEndPoint.Address + "and port: " + clientEndPoint.Port);
+    }
+    public void SendCommand()
+    {
+        if (inputField.text == "")
+            return;
+        if(clientSocket.Poll(0,SelectMode.SelectWrite))
+        {
+            Message msg = new Message();
+            if (state == ClientState.UnamedConnected)
+                msg.Type = Message.MessageType.ServerNameRequest;
+            else
+            {
+                msg.Type = Message.MessageType.ServerBroadcastMessageRequest;
+                msg.senderName = username;
+            }
+            msg.message = inputField.text;
+            inputField.text = "";
+            JSONSerializeAndSendMessage(msg, clientSocket);
+        }
+        else
+        {
+            OutputChat("Not connected to server");
+            state = ClientState.Disconected;
+        }
+    }
+    private void ConnectToserver()
+    {
+        //Peta si no hi ha server!!!!!
+        try
+        {
+            clientSocket.Connect(serverIpep);
+        }
+        catch(SocketException e)
+        {
+            //Server not active
+            if (e.SocketErrorCode == SocketError.ConnectionRefused)
+            {
+                lock(notActiveLock)
+                {
+                    serverNotActive = true;
+                }
+            }
+        }
+        int a = 3;
+    }
+    private void OutputChat(string output, string sender = "")
+    {
+        if (sender != "")
+            outputLog.text += sender + ": ";
+        outputLog.text += output + System.Environment.NewLine;
+    }
+}
--- Assets/Scripts/ChatServer.cs
+++ Assets/Scripts/ChatServer.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
+
+public struct Message
+{
+    public enum MessageType
+    {
+        ServerNameRequest,
+        ClientNameRejected,
+        ClientNameAccepted,
+        ClientNewClientOnChat,
+        ClientClientLeavedChat,
+        ServerBroadcastMessageRequest,
+        ClientBroadcastedMessage
+    }
+    public MessageType Type;
+    public string senderName;
+    //public System.DateTime date;
+    //Color color 
+    public string message;
+}
+
 public class ChatServer : MonoBehaviour
 {
     private Socket serverSocket;
     //What container do I use???
     //Remember to cast to Socket if using ArrayList!!!
     //ArrayList clients = new ArrayList();
-    private List<Socket> clients = new List<Socket>();
+    private List<Client> clients = new List<Client>();
+    private List<Message> msgsToBroadcast = new List<Message>();
 
     private class Client
     {
-        string name;
-        List<string> bannedClients;
-        Socket socket;
-    }
-
-    private class Message
-    {
-        enum MessageType
-        {
-            ServerHellow,
-            //ChangeNameServerRequest
-            BroadcastMessageRequest,
-            ServerChatUpdate
-        }
-        MessageType Type;
-        string senderName;
-        System.DateTime date;
-        string message;
+        public bool connected = false;
+        public string name;
+        public List<string> bannedClients;
+        public Socket socket;
+        //Color color
     }
 
     void Start()
@@ -45,42 +54,119 @@ public class ChatServer : MonoBehaviour
 
     void Update()
     {
-        while (serverSocket.Poll(1000, SelectMode.SelectRead))
+        while (serverSocket.Poll(0, SelectMode.SelectRead))
         {
-            Socket accepted = serverSocket.Accept();
+            Client accepted = new Client();
+            accepted.socket = serverSocket.Accept();
             clients.Add(accepted);
-            IPEndPoint clientEndPoint = (IPEndPoint)accepted.RemoteEndPoint;
+            IPEndPoint clientEndPoint = (IPEndPoint)accepted.socket.RemoteEndPoint;
             Debug.Log("Accepted new client with address: " + clientEndPoint.Address + "and port: " + clientEndPoint.Port);
         }
         for (int i = 0; i < clients.Count; ++i)
         {
-            while (clients[i].Poll(1000, SelectMode.SelectRead))
+            while (clients[i].socket.Poll(0, SelectMode.SelectRead))
             {
-                byte[] data = new byte[1024];
-                int recv = clients[i].Receive(data);
+                byte[] data = new byte[2048];
+                int recv = clients[i].socket.Receive(data);
                 if (recv == 0)
                 {
-                    IPEndPoint clientEndPoint = (IPEndPoint)clients[i].RemoteEndPoint;
+                    IPEndPoint clientEndPoint = (IPEndPoint)clients[i].socket.RemoteEndPoint;
                     Debug.Log("Client Disconected with address: " + clientEndPoint.Address + "and port: " + clientEndPoint.Port);
-                    clients[i].Close();
+                    Message msg = new Message();
+                    msg.Type = Message.MessageType.ClientClientLeavedChat;
+                    msg.message = clients[i].name;
+                    msgsToBroadcast.Add(msg);
+                    clients[i].socket.Close();
                     clients.RemoveAt(i);
                     --i;
                     break;
                 }
-                string received = Encoding.ASCII.GetString(data, 0, recv);
-                if (received == "Ping" && clients[i].Poll(1000, SelectMode.SelectWrite))
+                MemoryStream stream = new MemoryStream(data);
+                BinaryReader reader = new BinaryReader(stream);
+                string json;
+                //string json = Encoding.ASCII.GetString(data, 0, recv);
+                while (true)
                 {
-                    clients[i].Send(Encoding.ASCII.GetBytes("Pong"));
-                    Debug.Log("Sending pong to client");
+                    json = reader.ReadString();
+                    if (json == "")
+                        break;
+
+                    Message msg = new Message();
+                    msg = JsonUtility.FromJson<Message>(json);
+                    switch (msg.Type)
+                    {
+                        case Message.MessageType.ServerBroadcastMessageRequest:
+                            msg.Type = Message.MessageType.ClientBroadcastedMessage;
+                            msgsToBroadcast.Add(msg);
+                            break;
+                        case Message.MessageType.ServerNameRequest:
+                            if (msg.message.StartsWith("/") || UserNameInUse(msg.message))
+                            {
+                                msg.Type = Message.MessageType.ClientNameRejected;
+                                JSONSerializeAndSendMessage(msg, clients[i].socket);
+                            }
+                            else
+                            {
+                                msg.Type = Message.MessageType.ClientNameAccepted;
+                                JSONSerializeAndSendMessage(msg, clients[i].socket);
+                                msg.Type = Message.MessageType.ClientNewClientOnChat;
+                                msgsToBroadcast.Add(msg);
+                                clients[i].connected = true;
+                            }
+                            break;
+                        default:
+                            Debug.LogWarning("Received and unhandlable message type");
+                            break;
+                    }
+                    Debug.Log(msg.message);
                 }
             }
+        }
+        if (msgsToBroadcast.Count > 0)
+        {
+            foreach (Client client in clients)
+            {
+                if (client.connected)
+                {
+                    foreach (Message msg in msgsToBroadcast)
+                    {
+                        //What if we fail here unable to send the message because of the poll???
+                        //look here for the banned clients
+                        if (client.socket.Poll(0, SelectMode.SelectWrite))
+                            JSONSerializeAndSendMessage(msg, client.socket);
+                    }
+                }
+            }
+            msgsToBroadcast.Clear();
         }
     }
     private void OnDestroy()
     {
-        //Peta al fer el shutdown
-        //serverSocket.Shutdown(SocketShutdown.Both);
         serverSocket.Close();
         Debug.Log("Bye :)");
+    }
+    private void JSONSerializeAndSendMessage(Message message, Socket socket)
+    {
+        //The endiannes???
+        //chack with the poll if we can send the message: what happens if we can't????
+        string json = JsonUtility.ToJson(message);
+        MemoryStream stream = new MemoryStream();
+        BinaryWriter writer = new BinaryWriter(stream);
+        writer.Write(json);
+        //Encoding.UTF8.GetBytes(json)
+        socket.Send(stream.GetBuffer());
+        //socket.Send(Encoding.ASCII.GetBytes(json));
+        IPEndPoint clientEndPoint = (IPEndPoint)socket.RemoteEndPoint;
+        Debug.Log("Sending message to client with address: " + clientEndPoint.Address + " and port: " + clientEndPoint.Port);
+    }
+
+    private bool UserNameInUse(string name)
+    {
+        foreach (Client client in clients)
+        {
+            if (client.name == name)
+                return true;
+        }
+        return false;
     }
 }
